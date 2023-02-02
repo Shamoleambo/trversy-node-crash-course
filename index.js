5ae60d3dfa9143e45faa7b36f6a15a22b3cf9a8d
Tiago Gomes <tidgomes@gmail.com>
2023-02-02 10:26:35 -0300

    create server with simple response

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
+const server = http.createServer((req, res) => {
+  if(req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.end('<h1>Dome</h1>')
+  }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
