0999468a6e1e21d1d75faa85a7e26bce3997246f
Tiago Gomes <tidgomes@gmail.com>
2023-02-03 10:02:14 -0300

    refactor server logic

--- index.js
+++ index.js
@@ -3,35 +3,48 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if (err) throw err
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(content)
-      }
-    )
-  } else if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'about.html'),
-      (err, content) => {
-        if (err) throw err
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        res.end(content)
-      }
-    )
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  )
+  let extname = path.extname(filePath)
+  let contentType = 'text/html'
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.jpg':
+      contentType = 'image/jpg'
   }
 
-  if (req.url === '/api/users') {
-    const users = [
-      { name: 'Mano', age: 20 },
-      { name: 'Manolo', age: 22 }
-    ]
-
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(users))
-  }
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        //Page not found (most cases)
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, data) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' })
+          res.end(data, 'utf8')
+        })
+      } else {
+        res.writeHead(500)
+        res.end(`Server Error: ${err.code}`)
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType })
+      res.end(data, 'utf8')
+    }
+  })
 })
 
 const PORT = process.env.PORT || 5000
