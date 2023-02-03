25ca3b751d798f57649d089d2dba3dcc26b694e4
Tiago Gomes <tidgomes@gmail.com>
2023-02-03 09:33:40 -0300

    serve index and about html pages

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('<h1>Dome</h1>')
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw err
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(content)
+      }
+    )
+  } else if (req.url === '/about') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if (err) throw err
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+        res.end(content)
+      }
+    )
   }
 })
 
